ee89e34e0d2375898cb993337f4e82691c72bd97
EXRAZER <EXRAZER@icloud.com>
2024-01-21 14:05:52 +0700

    update

--- numeral/roman.py
+++ numeral/roman.py
@@ -2,28 +2,28 @@
 
 def to_roman(arabic_num):
     if not 0 < arabic_num < 4000:
-        raise ValueError("Input must be between 1 and 3888")
+        raise ValueError("Input must be between 1 and 3999")
 
-    roman_num = [
-        ("M", 1000),
-        ("CM", 900),
-        ("D", 500),
-        ("CD", 400),
-        ("C", 100),
-        ("XC", 90),
-        ("L", 50),
-        ("XL", 40),
-        ("X", 10),
-        ("IX", 9),
-        ("V", 5),
-        ("IV", 4),
-        ("I", 1)
-    ]
+    roman_numeral = ""
+    roman_numerals = {
+        1000: "M",
+        900: "CM",
+        500: "D",
+        400: "CD",
+        100: "C",
+        90: "XC",
+        50: "L",
+        40: "XL",
+        10: "X",
+        9: "IX",
+        5: "V",
+        4: "IV",
+        1: "I"
+    }
 
-    result = ""
-    for roman, value in roman_num:
+    for value, numeral in sorted(roman_numerals.items(), key=lambda x: x[0], reverse=True):
         while arabic_num >= value:
-            result += roman
+            roman_numeral += numeral
             arabic_num -= value
 
-    return result
+    return roman_numeral
