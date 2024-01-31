a2aed68ee6ff78dbecdb9d415a77d0dbbcefb87a
EXRAZER <EXRAZER@icloud.com>
2024-01-31 08:32:41 +0700

    update

--- tests/numeral/test_roman.py
+++ tests/numeral/test_roman.py
@@ -8,34 +8,56 @@ import pytest
 # Project library
 from numerals.roman import to_roman
 
-@pytest.mark.parametrize(
+def test_calling_function():
+    """Call a roman numeral function."""
+    
+
+class ToRomanTest:
+    """Test cases for to_roman()."""
+    @pytest.mark.parametrize(
     "arabic_num, roman_num",
     [
         (1, "I"),
-        (5, "V"),
-        (10, "X"),
-        (50, "L"),
-        (100, "C"),
-        (1000, "M"),
-        
-        # Addition case
-        (2, "II"),
-        (3, "III"),
-        (6, "VI"),
-        
-        # Subtraction
-        (4, "IV"),
-        (9, "IX"),
-        (3888, "MMMDCCCLXXXVIII"),
-        (3999, "MMMCMXCIX")
+        (5, "V")
     ]
-)
-def test_to_roman(arabic_num, roman_num):
-    """Test roman"""
-    # Arrange
+    )
+    def test_base_case(self, arabic_num, roman_num):
+        """Test roman"""
+        # Arrange
+        # Act
+        result = to_roman(arabic_num)
 
-    # Act
-    result = to_roman(arabic_num)
+        # Assert
+        assert result == roman_num
+        
+        @pytest.mark.parametrize(
+            "arabic_num, roman_num",
+            [
+                (2, "II"),
+                (6, "VI")
+            ]
+        )
+        def test_add_case(self, arabic_num, roman_num):
+            """Test roman"""
+            # Arrange
+            # Act
+            result = to_roman(arabic_num)
 
-    # Assert
-    assert result == roman_num
+            # Assert
+            assert result == roman_num
+            
+        @pytest.mark.parametrize(
+            "arabic_num, roman_num",
+            [
+            (4, "IV"),
+            (9, "IX")
+            ]
+        )
+        def test_sub_case(self, arabic_num, roman_num):
+            """Test roman"""
+            # Arrange
+            # Act
+            result = to_roman(arabic_num)
+
+            # Assert
+            assert result == roman_num
